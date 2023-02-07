1df123bc04a89e459b264d06c331e2172a2c9b6f
Charulatha Mani <charulathamani118@gmail.com>
2023-02-07 10:23:41 +0400

    omg

--- Week 16/service-worker.js
+++ Week 16/service-worker.js
@@ -25,11 +25,24 @@ self.addEventListener('install', (e) => {
 //     console.log('[Service Worker] Fetched resource '+e.request.url);
 // });
 
-self.addEventListener('fetch', function(e) {
+// self.addEventListener('fetch', function(e) {
+//     e.respondWith(
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//             return r
+//         })
+//     );
+// });
+
+self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (r) {
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
